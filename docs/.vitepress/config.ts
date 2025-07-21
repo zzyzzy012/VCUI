4914b08ab8c5edb56bda3c5f129f56b336772778
zzy012 <109944754+zzyzzy012@users.noreply.github.com>
2025-07-21 13:41:33 +0800

    修改vitepress文档页面

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,50 +4,40 @@ import { containerPreview, componentPreview } from '@vitepress-demo-preview/plug
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "VueElement组件",
-  description: "基于Vue3开发的，仿照ElementPlus的组件库",
+  title: 'VueElement组件',
+  description: '基于Vue3开发的，仿照ElementPlus的组件库',
   // srcDir: ".",
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '../../src'),  // @ => 项目根目录下的 src
-        '~': path.resolve(__dirname, '../../')       // ~ => 项目根目录
-      }
-    }
+        '@': path.resolve(__dirname, '../../src'), // @ => 项目根目录下的 src
+        '~': path.resolve(__dirname, '../../'), // ~ => 项目根目录
+      },
+    },
   },
   markdown: {
     config(md) {
       md.use(containerPreview)
       md.use(componentPreview)
-    }
+    },
   },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
+    outline: [2, 4],
+    outlineTitle: '本页目录',
+    nav: [{ text: '首页', link: '/' }],
     // 侧边栏
     sidebar: [
-      // {
-      //   text: 'Examples',
-      //   items: [
-      //     { text: 'Markdown Examples', link: '/markdown-examples' },
-      //     { text: 'Runtime API Examples', link: '/api-examples' }
-      //   ]
-      // },
       {
         text: 'Basic基础组件',
         items: [
           { text: 'Button 组件', link: '../components/button' },
-          { text: 'Icon 组件', link: '../components/icon' }
-        ]
+          { text: 'Icon 组件', link: '../components/icon' },
+        ],
       },
       {
         text: 'Data数据展示组件',
-        items: [
-          { text: 'Collapse 组件', link: '../components/collapse' },
-        ]
+        items: [{ text: 'Collapse 组件', link: '../components/collapse' }],
       },
       {
         text: 'Feedback 反馈组件',
@@ -55,24 +45,20 @@ export default defineConfig({
           { text: 'Alert 组件', link: '../components/alert' },
           { text: 'Tooltip 组件', link: '../components/tooltip' },
           { text: 'Message 组件', link: '../components/message' },
-        ]
+        ],
       },
       {
         text: 'Navigation导航组件',
-        items: [
-          { text: 'DropDown 组件', link: '../components/drop-down' },
-        ]
+        items: [{ text: 'DropDown 组件', link: '../components/drop-down' }],
       },
       {
         text: 'Form表单组件',
         items: [
           { text: 'Input 组件', link: '../components/input' },
-          { text: 'Switch 组件', link: '../components/switch' }
-        ]
+          { text: 'Switch 组件', link: '../components/switch' },
+        ],
       },
     ],
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+  },
 })
--- docs/index.md
+++ docs/index.md
@@ -3,23 +3,22 @@
 layout: home
 
 hero:
-  name: "My Awesome Project"
-  text: "A VitePress Site"
-  tagline: My great project tagline
+  name: 'VCUI组件库——仿ElementPlus'
+  text: 'VueElement组件'
+  tagline: VueElement
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: 组件库首页
+      link: /components/button.md
     - theme: alt
-      text: API Examples
+      text: 其他...
       link: /api-examples
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 实现组件
+    details: 按照组件功能进行分类，分为基础组件、数据展示组件、反馈组件、表单组件等，分别实现了Button组件、Collapse组件、Tooltip组件、Input组件等复杂组件
+  - title: Vitest 测试
+    details: 3.	组件开发完成后，分别使用Vitest编写测试用例进行单元测试
+  - title: Vitepress 搭建文档站点
+    details: 4.	使用VitePress搭建组件库文档站点，提供完整的文档和组件实例
 ---
-
